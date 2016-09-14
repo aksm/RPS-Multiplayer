dcde5dd5d2e96db15b53afcf4fa9c33221d8bb9a
aksm <albert.min@gmail.com>
2016-09-14 18:29:58 -0400

    Init files

---
+++ assets/js/config.js
@@ -0,0 +1,8 @@
+var config = {
+  apiKey: "AIzaSyAQf6fbF3fgJ3_yNBqJjy7Rw6EVq4upxag",
+  authDomain: "rps-multiplayer-fb9c7.firebaseapp.com",
+  databaseURL: "https://rps-multiplayer-fb9c7.firebaseio.com",
+  storageBucket: "rps-multiplayer-fb9c7.appspot.com",
+  messagingSenderId: "165847398149"
+};
+firebase.initializeApp(config);
