3827635ba73a3076fc4ee487a83bb7bf8420f52e
aksm <albert.min@gmail.com>
2016-09-21 22:25:57 -0400

    Update chat window auto-scroll and commenting

--- assets/js/chat.js
+++ assets/js/chat.js
@@ -1,5 +1,14 @@
 $(document).ready(function() {
+	// Local variable for current number of messages
 	var mnumber = 0;
+
+	// Scroll chat window to last message
+	function autoscroll() {
+		var height = $("#chat .panel-body")[0].scrollHeight;
+		$("#chat .panel-body").scrollTop(height);
+	}
+
+	// Click event handler to capture chat text and upload to firebase
 	$(document).on("click", "#chat button", function() {
 		if($("#chat .form-control").val() != "") {
 			mnumber++;
@@ -14,6 +23,8 @@ $(document).ready(function() {
 			$("#chat .form-control").val("");
 		}
 	});
+
+	// Firebase snapshot to display uploaded chat text in div
 	database.ref().on("value", function(snapshot) {
 		if(gamekey != "") {
 			database.ref().orderByKey().equalTo(gamekey).on("child_added", function(snapshot) {
@@ -25,8 +36,10 @@ $(document).ready(function() {
 				 		database.ref(gamekey).update(chatstatus);
 				 		if((role == "player1" && username == snapshot.val().chat["m"+mnumber].player) || (role == "player2" && opponentname == snapshot.val().chat["m"+mnumber].player)) {
 				 			$("#chat-display").append("<p class='text-left chat-user'>"+snapshot.val().chat["m"+mnumber].player+"<p class='text-left chat-text'>"+snapshot.val().chat["m"+mnumber].text+"<hr>");
+				 			autoscroll();
 						} else if((role == "player2" && username == snapshot.val().chat["m"+mnumber].player) || (role == "player1" && opponentname == snapshot.val().chat["m"+mnumber].player)) {
-				 			$("#chat-display").append("<p class='text-right chat-user'>"+snapshot.val().chat["m"+mnumber].player+"<p class='text-right chat-text'>"+snapshot.val().chat["m"+mnumber].text+"<hr>");							
+				 			$("#chat-display").append("<p class='text-right chat-user'>"+snapshot.val().chat["m"+mnumber].player+"<p class='text-right chat-text'>"+snapshot.val().chat["m"+mnumber].text+"<hr>");
+				 			autoscroll();
 						}
 					}
 				}
--- assets/js/game.js
+++ assets/js/game.js
@@ -16,18 +16,26 @@ var ties = 0;
 var gamekey = "";
 var round = 1;
 
+// Initialize functions
+
 // Pick image for rock, paper, or scissors
 function pickImage(token) {
 	return "assets/images/"+token+(Math.floor(Math.random() * (Math.floor(9) - Math.ceil(1) + 1)) + Math.ceil(1)) + ".gif";
 }
+
+// Hide divs as necessary
 function hide(element) {
 	element.removeClass("show");
 	element.addClass("hide");
 }
+
+// Show divs as necessary
 function show(element) {
 	element.removeClass("hide");
 	element.addClass("show");	
 }
+
+// End round and start new round
 function newRound(snapshot) {
 	if(snapshot.val().round == "complete") {
 		roundEnd(snapshot);				
@@ -37,12 +45,16 @@ function newRound(snapshot) {
 		setTimeout(function() {rebootRound(snapshot)}, 5000);
 	}
 }
+
+// Refresh new round elements
 function rebootRound(snapshot) {
-		$(".rps").prop("disabled", false);
-		show($("#"+role+" .tokens"));
-		$("#game .panel-heading").html("ROUND "+round);
-		$("#game .panel-body").html("<img src='"+pickImage("question")+"' class='img-responsive img-circle'>")
+	$(".rps").prop("disabled", false);
+	show($("#"+role+" .tokens"));
+	$("#game .panel-heading").html("ROUND "+round);
+	$("#game .panel-body").html("<img src='"+pickImage("question")+"' class='img-responsive img-circle'>")
 }
+
+// Update local variables for player stats and update firebase
 function player1Wins(snapshot) {
 	p1wins = snapshot.val().player1.wins;
 	p2losses = snapshot.val().player2.losses;
@@ -55,6 +67,8 @@ function player1Wins(snapshot) {
 		roundWinner: "PLAYER 1 WINS!!"
 	});
 }
+
+// Update local variables for player stats and update firebase
 function player2Wins(snapshot) {
 	p2wins = snapshot.val().player2.wins;
 	p1losses = snapshot.val().player1.losses;
@@ -67,6 +81,8 @@ function player2Wins(snapshot) {
 		roundWinner: "PLAYER 2 WINS!!"
 	});
 }
+
+// Update end of round elements
 function roundEnd(snapshot) {
 	var p1img = snapshot.val().player1.img;
 	var p2img = snapshot.val().player2.img;
@@ -74,12 +90,14 @@ function roundEnd(snapshot) {
 	$("#game .panel-heading").html(roundWinner);
 	$("#game .panel-body").html("<img src='"+p1img+"' class='img-responsive img-circle'><img src='"+p2img+"' class='img-responsive img-circle'>");
 }
+
 $(document).ready(function() {
 
 	// Initialize placeholder image for players
 	$("#player1 .question").html("<img src='"+pickImage("question")+"' class='img-responsive img-circle'>")
 	$("#player2 .question").html("<img src='"+pickImage("question")+"' class='img-responsive img-circle'>")
 	$("#game .panel-body").html("<img src='"+pickImage("question")+"' class='img-responsive img-circle'>")
+
 	// Update any waiting player in html and change button text
 	database.ref().orderByKey().limitToLast(1).on("child_added", function(snapshot) {
 		if(snapshot.val().players == 1) {
@@ -89,9 +107,11 @@ $(document).ready(function() {
 			$("#start-game").text("Join Game");
 		}
 	});
-	// Update local variables and firebase when player starts or joins game
+
+	// Update local variables, dom, and firebase when player starts or joins game
 	$(document).on("click","#start-game", function() {
 		database.ref().orderByKey().limitToLast(1).once("child_added", function(snapshot) {
+			// Update player 1 dom
 			if ((!snapshot.exists() || snapshot.val().players == 2) && playerState != "joined" && $("#username").val() != "") {
 				username = $("#username").val();
 				role = "player1";
@@ -100,11 +120,11 @@ $(document).ready(function() {
 				playerState = "joined";
 				hide($("#start-section"));
 				hide($("#player1 .question"));
-				$("#player1 .rock").html("<button class='btn btn-default rps' data-token='rock'><img src='"+pickImage("rock")+"' class='img-responsive img-circle'>");
-				$("#player1 .paper").html("<button class='btn btn-default rps' data-token='paper'><img src='"+pickImage("paper")+"' class='img-responsive img-circle'>");
-				$("#player1 .scissors").html("<button class='btn btn-default rps' data-token='scissors'><img src='"+pickImage("scissors")+"' class='img-responsive img-circle'>");
-				$("#player1 .lizard").html("<button class='btn btn-default rps' data-token='lizard'><img src='"+pickImage("lizard")+"' class='img-responsive img-circle'>");
-				$("#player1 .spock").html("<button class='btn btn-default rps' data-token='spock'><img src='"+pickImage("spock")+"' class='img-responsive img-circle'>");
+				$("#player1 .rock").html("<button class='btn btn-default rps' data-token='rock'><img src='"+pickImage("rock")+"' class='img-responsive img-circle col-xs-12 col-sm-12 col-md-12'>");
+				$("#player1 .paper").html("<button class='btn btn-default rps' data-token='paper'><img src='"+pickImage("paper")+"' class='img-responsive img-circle col-xs-12 col-sm-12 col-md-12'>");
+				$("#player1 .scissors").html("<button class='btn btn-default rps' data-token='scissors'><img src='"+pickImage("scissors")+"' class='img-responsive img-circle col-xs-12 col-sm-12 col-md-12'>");
+				$("#player1 .lizard").html("<button class='btn btn-default rps' data-token='lizard'><img src='"+pickImage("lizard")+"' class='img-responsive img-circle col-xs-12 col-sm-12 col-md-12'>");
+				$("#player1 .spock").html("<button class='btn btn-default rps' data-token='spock'><img src='"+pickImage("spock")+"' class='img-responsive img-circle col-xs-12 col-sm-12 col-md-12'>");
 				show($("#player1 .rock"));
 				show($("#player1 .paper"));
 				show($("#player1 .scissors"));
@@ -118,6 +138,7 @@ $(document).ready(function() {
 					dateAdded: firebase.database.ServerValue.TIMESTAMP
 				});
 				gamekey = newkey.getKey();
+				// Update player 2 dom
 			} else if (snapshot.val().players == 1 && playerState != "joined" && $("#username").val() != "") {
 				gamekey = snapshot.getKey();
 				username = $("#username").val();
@@ -127,11 +148,11 @@ $(document).ready(function() {
 				$("#player2-status").html("PLAYER 2: "+username);
 				hide($("#start-section"));
 				hide($("#player2 .question"));
-				$("#player2 .rock").html("<button class='btn btn-default rps' data-token='rock'><img src='"+pickImage("rock")+"' class='img-responsive img-circle'>");
-				$("#player2 .paper").html("<button class='btn btn-default rps' data-token='paper'><img src='"+pickImage("paper")+"' class='img-responsive img-circle'>");
-				$("#player2 .scissors").html("<button class='btn btn-default rps' data-token='scissors'><img src='"+pickImage("scissors")+"' class='img-responsive img-circle'>");
-				$("#player2 .lizard").html("<button class='btn btn-default rps' data-token='lizard'><img src='"+pickImage("lizard")+"' class='img-responsive img-circle'>");
-				$("#player2 .spock").html("<button class='btn btn-default rps' data-token='spock'><img src='"+pickImage("spock")+"' class='img-responsive img-circle'>");
+				$("#player2 .rock").html("<button class='btn btn-default rps' data-token='rock'><img src='"+pickImage("rock")+"' class='img-responsive img-circle col-xs-12 col-sm-12 col-md-12'>");
+				$("#player2 .paper").html("<button class='btn btn-default rps' data-token='paper'><img src='"+pickImage("paper")+"' class='img-responsive img-circle col-xs-12 col-sm-12 col-md-12'>");
+				$("#player2 .scissors").html("<button class='btn btn-default rps' data-token='scissors'><img src='"+pickImage("scissors")+"' class='img-responsive img-circle col-xs-12 col-sm-12 col-md-12'>");
+				$("#player2 .lizard").html("<button class='btn btn-default rps' data-token='lizard'><img src='"+pickImage("lizard")+"' class='img-responsive img-circle col-xs-12 col-sm-12 col-md-12'>");
+				$("#player2 .spock").html("<button class='btn btn-default rps' data-token='spock'><img src='"+pickImage("spock")+"' class='img-responsive img-circle col-xs-12 col-sm-12 col-md-12'>");
 				show($("#player2 .rock"));
 				show($("#player2 .paper"));
 				show($("#player2 .scissors"));				
@@ -156,6 +177,7 @@ $(document).ready(function() {
 		choicekey[role+"/round"+round] = choice;
 		choicekey[role+"/img"] = choiceImg;
 		database.ref(gamekey).update(choicekey);
+		// Disable button when choice is made and hide other choices
 		$(".rps").prop("disabled", true);
 		if(choice == "rock") {
 			hide($("#"+role+" .paper"));
@@ -184,7 +206,7 @@ $(document).ready(function() {
 			hide($("#"+role+" .lizard"));
 		}
 
-
+		// Grab player choices from firebase and determine winner. Call win functions to update variables and firebase.
 		database.ref(gamekey).once("value", function(snapshot) {
 			if(snapshot.child("player1/round"+round).exists() && snapshot.child("player2/round"+round).exists()) {
 				var p1token = snapshot.val().player1["round"+round];
@@ -238,20 +260,11 @@ $(document).ready(function() {
 				} else if(p1token == "scissors" && p2token == "spock") {
 					player2Wins(snapshot);
 				}
-
-
-
-
-
-
-
-
-
-
-
 			}
 		});
 	});
+
+	// Grab winner/loser info from firebase show in dom and call new round function
 	database.ref().on("value", function(snapshot) {
 		if(gamekey != "") {
 			database.ref(gamekey).on("value", function(snapshot) {
